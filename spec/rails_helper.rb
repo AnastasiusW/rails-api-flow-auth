8751fb364b055cac1ae500ada9daab0a6adeae53
AnastasiusW <repositoryanastasiia@gmail.com>
2021-07-13 15:29:21 +0300

    Repair rails_helper2

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require 'rspec/rails'
 
 require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 begin
   ActiveRecord::Migration.maintain_test_schema!
