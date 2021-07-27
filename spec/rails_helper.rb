2d1675034040be9a0f960847c3cc1759601a7cbb
AnastasiusW <repositoryanastasiia@gmail.com>
2021-07-27 13:31:02 +0300

    Add simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/spec/'
+  minimum_coverage 90
+end
+
 require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
